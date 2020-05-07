7e7bf7291c3402b0e7266ffcb1197f7078cd0485
Caiua Gomes de Franca <caiua.franca@ale.com.br>
2020-05-07 08:01:52 -0300

    Cast de variavel

--- Modulo01/main.go
+++ Modulo01/main.go
@@ -23,9 +23,16 @@ func main() {
 	Y = "James Bond"
 	Z = true
 	C = 20
+	var x2 = cursoEllen(x)
 
 	fmt.Printf("Bem Vindo ao Mundo dos gophers, %d, %s, %v \n", x, Y, Z)
 	saida := fmt.Sprintf("%T", C)
-	fmt.Printf("o dado e do tipo %s e tem valor %d", saida, C)
+	fmt.Printf("o dado e do tipo %s e tem valor %d \n", saida, C)
+
+	fmt.Println("Cast do tipo")
+	saida2 := fmt.Sprintf("%T", x2)
+	fmt.Printf("o dado e do tipo %s e tem valor %d \n", saida2, x2)
+
 	fmt.Println("Vamos ao Nível Ninja - Level 01")
+
 }
